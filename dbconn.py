5316c6aafb9b0bbe8b6e3304b48423b1fd70764e
zzy93421 <zzy93421@163.com>
2017-02-28 21:01:16 +0800

    add xgp database connect

--- dbconn.py
+++ dbconn.py
@@ -4,12 +4,14 @@ Created on 2015年9月17日
 @author: zhang zhiyuan
 '''
 import sqlite3
-# import cx_Oracle
+import cx_Oracle
+
 
 class Sqlite3Conn:
     '''
     连接sqlite3的通用类
     '''
+
     def __init__(self, dbfile=r'D:\HFOrder\high_frequence.db'):
         self.dbstr = dbfile
         self.sqlite3_conn = sqlite3.connect(self.dbstr)
@@ -29,4 +31,16 @@ class Sqlite3Conn:
         '''
         self.sqlite3_conn = sqlite3.connect(self.dbstr)
         self.sqlite3_cur = self.sqlite3_conn.cursor()
-        
+
+
+class XGPDBConn():
+    '''
+             连接高频数据库类
+    '''
+
+    def __init__(self, tns, username, password):
+        self.xgpdbconn_input_dict = {
+            'username': ('helios' if not username else username),
+            'password': ('helios' if not password else password),
+            'tns': (r'//192.168.80.21/xgp1' if not tns else tns),
+        }
